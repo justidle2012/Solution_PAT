a7d946c0349261587a970b3a1dee3eeaae679ee4
justidle2012 <1540168075@qq.com>
2019-11-07 21:37:18 +0800

    简单修改

--- Basic Level/1019/gen.cpp
+++ Basic Level/1019/gen.cpp
@@ -1,6 +1,5 @@
 //生成数据
 #include <cstdio>
-#include <sstream>
 #include <cstdlib>
 #include <ctime>
 
@@ -8,14 +7,7 @@ int main(int argc, char *argv[]) {
     int seed = time(NULL);
     if (argc>1) {
         //有参数
-#if 1
-        std::stringstream ss;
-        ss.clear();
-        ss<<argv[1];
-        ss>>seed;
-#else    
         seed = atoi(argv[1]);
-#endif        
     }
     srand(seed);
 
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # PATSolution
 PAT题解
+
+PAT的官方网站为https://www.patest.cn/practice。目前PAT包括三个层次：乙级（Basic Level）、甲级（Advanced Level）和顶级（Top Level）。
+
+我的目标是逐步将这三级题库中的样题完成，并写出题解报告。
